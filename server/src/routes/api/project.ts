f3889e7138cb8a30f1b82ac5d172abc7b84851c5
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-04-26 02:40:16 +0600

    added delete project

--- server/src/routes/api/project.ts
+++ server/src/routes/api/project.ts
@@ -24,4 +24,36 @@ router.get('/get-all-projects', authenticateToken, getAllProjectsForAllClientsCo
 //update a project by id
 router.put('/update-project/:id', authenticateToken, updateProjectController);
 
+//delete project
+router.delete(
+  '/delete-project/:id',
+  authenticateToken,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { id: projectId } = req.params;
+      if (!Types.ObjectId.isValid(projectId)) {
+        res.status(400).json({ message: 'Invalid projectId format' });
+        return;
+      }
+
+      const clients = await Client.find({ user: req.user!._id }).select('_id');
+      const clientIds = clients.map((client) => client._id);
+
+      const projectToDelete = await Project.findOneAndDelete({
+        _id: projectId,
+        client: { $in: clientIds },
+      });
+
+      if (!projectToDelete) {
+        res.status(404).json({ message: 'Project not found or unauthorized access' });
+        return;
+      }
+      res.status(200).json({ message: 'Project deleted successfully' });
+    } catch (error) {
+      console.error('Error deleting project:', error);
+      res.status(500).json({ message: 'Server error' });
+    }
+  }
+);
+
 export default router;
