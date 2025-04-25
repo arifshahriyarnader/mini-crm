84b70eb9bdac2da5952f7fd9bbf2b52f85dad195
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-04-26 01:35:29 +0600

    added get all projects for all clients

--- server/src/routes/api/project.ts
+++ server/src/routes/api/project.ts
@@ -5,6 +5,7 @@ import {
   createProjectController,
   getAllProjectsByClientIdController,
 } from '../../controllers/project.controller';
+import { Client, Project } from '../../models';
 
 const router = Router();
 
@@ -13,5 +14,20 @@ router.post('/create-project', authenticateToken, createProjectController);
 
 //get all projects for a client by id
 router.get('/get-all-projects/:clientId', authenticateToken, getAllProjectsByClientIdController);
+
+//get all projects
+router.get('/get-all-projects', authenticateToken, async (req: Request, res: Response) => {
+  try {
+    const clients = await Client.find({ user: req.user!._id }).select('_id');
+    const clientIds = clients.map(client => client._id);
+    const projects = await Project.find({ client: { $in: clientIds } }).populate('client');
+    res.status(200).json({ message: 'Projects fetched successfully', projects });
+
+  } catch (error) {
+    console.error('Error fetching projects for user:', error);
+    res.status(500).json({ message: 'Server error' });
+  }
+});
+
 
 export default router;
