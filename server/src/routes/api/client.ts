0361941b9e107e5f6d1c71dc9411bc142f01c11d
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-04-25 14:41:15 +0600

    added delete a specific client by id

--- server/src/routes/api/client.ts
+++ server/src/routes/api/client.ts
@@ -22,4 +22,25 @@ router.get('/client/:id', authenticateToken, getClientbyIdController);
 //update a client by id
 router.put('/client/:id', authenticateToken, updateClientByIdController);
 
+//delete a client by id
+router.delete(
+  '/delete-client/:id',
+  authenticateToken,
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const clientId = req.params.id;
+      const userId = req.user!._id;
+      const client = await Client.findByIdAndDelete({ _id: clientId, userId });
+      if (!client) {
+        res.status(404).json({ message: 'Client not found' });
+        return;
+      }
+      res.status(200).json({ message: 'Client deleted successfully' });
+    } catch (error) {
+      console.error('Error deleting client:', error);
+      res.status(500).json({ error: 'Server error' });
+    }
+  }
+);
+
 export default router;
