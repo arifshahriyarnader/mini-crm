c055be730c2447449586aef6b9112cb451ab048d
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-04-25 11:33:31 +0600

    added get a specific client by id

--- server/src/routes/api/client.ts
+++ server/src/routes/api/client.ts
@@ -14,4 +14,21 @@ router.post('/create-client', authenticateToken, createClientController);
 //get all clients
 router.get('/all-clients', authenticateToken, getAllClientsController);
 
+//get a client by id
+router.get('/client/:id', authenticateToken, async (req: Request, res: Response): Promise<void> => {
+  try {
+    const clientId = req.params.id;
+    const userId = req.user!._id;
+    const client = await Client.findById({ _id: clientId, userId });
+    if (!client) {
+      res.status(404).json({ error: 'Client not found' });
+      return;
+    }
+    res.status(200).json(client);
+  } catch (error) {
+    console.error('Error fetching client:', error);
+    res.status(500).json({ error: 'Server error' });
+  }
+});
+
 export default router;
