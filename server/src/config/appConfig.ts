759ec7a519eb99879008ba2356ebbc3a64237a63
Arif Shahriyar Nader <arif.nader95@gmail.com>
2025-04-29 07:30:43 +0600

    dockerized server and client

--- server/src/config/appConfig.ts
+++ server/src/config/appConfig.ts
@@ -3,11 +3,11 @@ dotenv.config();
 
 export const appConfig = {
     PORT: process.env.PORT || 5000,
-    ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || 'http://localhost:5173',
+    ALLOWED_ORIGIN: process.env.ALLOWED_ORIGIN || 'http://client:5173',
     DB:{
-        MONGODB_URI: process.env.MONGODB_URI
+        MONGODB_URI: process.env.MONGODB_URI || 'mongodb://root:example@mongo:27017/crm-mini?authSource=admin'
     },
     AUTH:{
-        JWT_SECRET:process.env.JWT_SECRET as string,
+        JWT_SECRET:process.env.JWT_SECRET || 'my_jwt_secret_key' as string,
     }
 }
